4f2b7cd4d848c953bce9febe8658c73f8f8b4bfe
bea_ek <ekaterinaboldashova@yandex.com>
2024-12-03 16:46:59 +0500

    .

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
-def main():
-    print('My first git program')
+import sys
+
+from PyQt6 import uic
+from PyQt6.QtGui import QPainter, QColor
+from PyQt6.QtWidgets import QApplication, QMainWindow
+
+
+class WidgetArt(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.pushButton.clicked.connect(self.run)
+
+    def run(self):
+        pass
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
 
 
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+    ex = WidgetArt()
+    ex.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec())
