356644ae6aef7dd0018a3fe9a7746e7a4773ed82
bea_ek <ekaterinaboldashova@yandex.com>
2024-12-03 16:58:52 +0500

    .

--- main.py
+++ main.py
@@ -3,16 +3,32 @@ import sys
 from PyQt6 import uic
 from PyQt6.QtGui import QPainter, QColor
 from PyQt6.QtWidgets import QApplication, QMainWindow
+from random import *
 
 
 class WidgetArt(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('UI.ui', self)
-        self.pushButton.clicked.connect(self.run)
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
 
-    def run(self):
-        pass
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_circle(qp)
+            qp.end()
+        self.do_paint = False
+
+    def paint(self):
+        self.do_paint = True
+        self.update()
+
+    def draw_circle(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        r = randint(1,100)
+        qp.drawEllipse(200, 200, r, r)
 
 
 def except_hook(cls, exception, traceback):
